ad7cc9f358ffea6ad674eb7411fa1011630319ea
koome <koomemc@gmail.com>
2024-03-07 21:24:24 +0300

    change to arrow functions

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,10 +4,10 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars() {
+    getIncomeInDollars: () => {
       return `$${this.income}`;
     },
-    getIncomeInEuros() {
+    getIncomeInEuros: () => {
       return `${this.income} euros`;
     },
   };
