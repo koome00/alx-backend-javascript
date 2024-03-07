2db42cc3ba9ad2cacbd02ae23f68995b2b7c35ce
koome <koomemc@gmail.com>
2024-03-07 21:20:03 +0300

    change function

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,10 +4,10 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: function () {
+    getIncomeInDollars() {
       return `$${this.income}`;
     },
-    getIncomeInEuros: function ()  {
+    getIncomeInEuros() {
       return `${this.income} euros`;
     },
   };
